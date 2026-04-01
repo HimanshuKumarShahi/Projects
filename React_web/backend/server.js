0f2af27f7708c6a2c0c26ac69024d97f7582fd8d
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-04-01 12:37:52 +0530

    Testing server

--- React_web/backend/server.js
+++ React_web/backend/server.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
-app.listen(5000 , (req , res)=>{
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("API is Running .")
+})
+
+
+const PORT=process.env.PORT || 5000
+app.listen(PORT  , ()=>{
     console.log("Running .....");
     
 })
