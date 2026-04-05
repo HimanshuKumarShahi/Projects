45e558c37f5d20115726be72aeaa1fddad17f555
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-04-05 14:27:58 +0530

    Connect DataBase in server.

--- React_web/backend/server.js
+++ React_web/backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import ConnectDB from './config/db.js';
 
 const app = express()
 
 
 app.use(cors());
 app.use(express.json());
+
+ConnectDB();
 
 app.get('/',(req,res)=>{
     res.send("API is Running .")
